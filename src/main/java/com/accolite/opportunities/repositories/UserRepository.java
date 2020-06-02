7b2547567b482339499689feee9b2eb26e9ceaaa
Deepak Pandey <pandey.deepak565@gmail.com>
2020-06-02 13:05:15 +0530

    remove unnecessary files

--- src/main/java/com/accolite/opportunities/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.accolite.opportunities.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.accolite.opportunities.entities.User;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long>{}
